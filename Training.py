5f813cd48ed62bb3e1d74c86119574b1d3639421
lcouturier <lcouturier75@free.fr>
2017-12-26 08:59:41 +0100

    Création du projet

---
+++ Training.py
@@ -0,0 +1,36 @@
+# Exercises
+
+
+def first_occuring_char(value):
+    dic = {}
+    for c in value:
+        if c in dic:
+            return c
+        dic[c] = 1
+
+
+def max_occuring_char(value):
+    dic = {}
+    i = 0
+    previous = value[0]
+    for c in value:
+        if c in dic:
+            if c == previous:
+                dic[c] = dic[c] + 1
+            else:
+                dic[c] = 1
+        else:
+            dic[c] = 1
+        i = i + 1
+        previous = value[i - 1]
+
+    d = {v: k for k, v in dic.items()}
+    return d[max(d)]
+
+
+def main():
+    result = max_occuring_char("AABBBDDADDDDD")
+
+
+if __name__ == '__main__':
+    main()
