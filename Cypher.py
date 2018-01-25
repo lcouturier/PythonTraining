4e4a1447036d8ea67e1db5976b981d2ad6946e99
lcouturier <lcouturier75@free.fr>
2018-01-25 15:15:27 +0100

    Ajout de la création de graphique

--- Cypher.py
+++ Cypher.py
@@ -1,11 +1,14 @@
+def next_letter(c, key):
+    if ord(c) + key <= 90:
+        return chr((ord(c) + key))
+
+    return chr((((ord(c) + key) - 90) + 65) - 1)
+
 def cipher_wheel(phrase, key):
     result = ""
     for c in phrase.upper():
         if c != " ":
-            if ord(c) + key <= 90:
-                result = result + chr((ord(c) + key))
-            else:
-                result = result + chr((((ord(c) + key) - 90) + 65) - 1)
+            result = result + next_letter(c, key)
         else:
             result = result + c
     return result
