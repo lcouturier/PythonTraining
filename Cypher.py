458be3e44c476383524ef118b5eca49ee85bdb26
lcouturier <lcouturier75@free.fr>
2018-01-26 16:49:54 +0100

    Ajout de la création de graphique

--- Cypher.py
+++ Cypher.py
@@ -2,13 +2,31 @@ def next_letter(c, key):
     if ord(c) + key <= 90:
         return chr((ord(c) + key))
 
-    return chr((((ord(c) + key) - 90) + 65) - 1)
+    return chr((ord(c) + key) - 26)
 
-def cipher_wheel(phrase, key):
+
+def previous_letter(c, key):
+    if ord(c) - key >= 65:
+        return chr((ord(c) - key))
+
+    return chr((ord(c) - key) + 26)
+
+
+def cipher_wheel_crypt(phrase, key):
     result = ""
     for c in phrase.upper():
         if c != " ":
             result = result + next_letter(c, key)
+        else:
+            result = result + c
+    return result
+
+
+def cipher_wheel_decrypt(phrase, key):
+    result = ""
+    for c in phrase.upper():
+        if c != " ":
+            result = result + previous_letter(c, key)
         else:
             result = result + c
     return result
